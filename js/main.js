d0c345734aff4c432d71fb7e0c3a34c1e56e1cc9
Karina <karinamaulitova@gmail.com>
2019-07-02 22:18:34 +0300

    Сделана валидация формы на странице

--- js/main.js
+++ js/main.js
@@ -70,7 +70,6 @@ var removeSimilarAds = function () {
   similarAdsElements = [];
 };
 
-
 var activateMap = function () {
   document.querySelector('.map').classList.remove('map--faded');
 
@@ -94,12 +93,14 @@ var activateAdForm = function () {
   document.querySelectorAll('.ad-form fieldset').forEach(function (element) {
     element.disabled = false;
   });
+  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
 };
 
 var deactivateAdForm = function () {
   document.querySelectorAll('.ad-form fieldset').forEach(function (element) {
     element.disabled = true;
   });
+  document.querySelector('.ad-form').classList.add('ad-form--disabled');
 };
 
 
@@ -146,4 +147,40 @@ resetButton.addEventListener('click', function (evt) {
   if (keksobookingActive) {
     deActivateKeksobooking();
   }
+});
+
+var typeSelect = document.querySelector('#type');
+var priceInput = document.querySelector('#price');
+
+typeSelect.addEventListener('change', function (evt) {
+  var selectValue = evt.target.value;
+  switch (selectValue) {
+    case 'bungalo':
+      priceInput.setAttribute('placeholder', '0');
+      priceInput.setAttribute('min', '0');
+      break;
+    case 'flat':
+      priceInput.setAttribute('placeholder', '1000');
+      priceInput.setAttribute('min', '1000');
+      break;
+    case 'house':
+      priceInput.setAttribute('placeholder', '5000');
+      priceInput.setAttribute('min', '5000');
+      break;
+    case 'palace':
+      priceInput.setAttribute('placeholder', '10000');
+      priceInput.setAttribute('min', '10000');
+      break;
+  }
+});
+
+var timeInSelect = document.querySelector('#timein');
+var timeOutSelect = document.querySelector('#timeout');
+
+timeInSelect.addEventListener('change', function () {
+  timeOutSelect.value = timeInSelect.value;
+});
+
+timeOutSelect.addEventListener('change', function () {
+  timeInSelect.value = timeOutSelect.value;
 });
