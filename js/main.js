1b81e6bada72a7fb975abd27a7b1797aa47cf124
Karina <karinamaulitova@gmail.com>
2019-07-03 23:42:47 +0300

    Добалвена возможность перемещать метку + запись координат хвостика метки в форму

--- js/main.js
+++ js/main.js
@@ -126,20 +126,75 @@ var setAddress = function (x, y) {
 };
 
 var mapPinMain = document.querySelector('.map__pin--main');
-var mapPinMainWidth = mapPinMain.offsetWidth;
-var mapPinMainHeight = mapPinMain.offsetHeight;
+var mapPinMainRect = mapPinMain.getBoundingClientRect();
+var mapPinMainWidth = mapPinMainRect.width;
+var mapPinMainHeight = mapPinMainRect.height + 20;
 var defaultAddressX = mapPinMain.offsetLeft + mapPinMainWidth / 2;
 var defaultAddressY = mapPinMain.offsetTop + mapPinMainHeight / 2;
 setAddress(defaultAddressX, defaultAddressY);
 
-mapPinMain.addEventListener('mouseup', function (evt) {
-  setAddress(evt.clientX, evt.clientY);
+mapPinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
 
   if (!keksobookingActive) {
     activateKeksobooking();
   }
 
+  var onMouseMove = function (moveEvt) {
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var newTopCoordinate = mapPinMain.offsetTop - shift.y;
+    var newLeftCoordinate = mapPinMain.offsetLeft - shift.x;
+
+    var mapRect = document.querySelector('.map').getBoundingClientRect();
+
+
+    var rightBorder = mapRect.width - mapPinMainWidth;
+    var bottomBorder = mapRect.height - mapPinMainHeight;
+
+    if (newTopCoordinate > bottomBorder) {
+      newTopCoordinate = bottomBorder;
+    } else if (newTopCoordinate < 0) {
+      newTopCoordinate = 0;
+    }
+
+    if (newLeftCoordinate > rightBorder) {
+      newLeftCoordinate = rightBorder;
+    } else if (newLeftCoordinate < 0) {
+      newLeftCoordinate = 0;
+    }
+
+    setAddress(newLeftCoordinate + mapPinMainWidth / 2, newTopCoordinate + mapPinMainHeight);
+
+    mapPinMain.style.top = newTopCoordinate + 'px';
+    mapPinMain.style.left = newLeftCoordinate + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    setAddress(mapPinMain.offsetLeft + mapPinMainWidth / 2, mapPinMain.offsetTop + mapPinMainHeight);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
+
 
 var resetButton = document.querySelector('.ad-form__reset');
 resetButton.addEventListener('click', function (evt) {
